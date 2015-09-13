ab0ab35f2cfec6847c381c4db513c1ca19af4b20
Nathan Prentice <nathanprentice830@gmail.com>
2015-09-13 22:24:00 +0100

    Create WritingLines.java

---
+++ src/WritingLines.java
@@ -0,0 +1,21 @@
+
+public class WritingLines {
+
+	public static void main(String[] args){
+		int count;	//sets up a variable that manages how many times the line has been written
+		int numberOfLines = 100;	// sets the number of lines
+		
+		
+		/*	Set count equal to 0
+		 * 	Execute while counter is less than number of lines
+		 * 	Increment count by one
+		*/
+		
+		for (count = 0; count < numberOfLines; count++){
+			System.out.println("I will not talk in class");
+			
+		}
+
+	}
+
+}
